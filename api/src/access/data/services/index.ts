8de530b3b7faf069bdc534f8fc26144b75c6ce07
PorfirioR <porfirioperezgon@gmail.com>
2025-10-08 03:25:54 -0300

    62-implement-backend-transactions-workflow

--- api/src/access/data/services/index.ts
+++ api/src/access/data/services/index.ts
@@ -1,12 +1,11 @@
+export { DbContextService } from './db-context.service';
+export { BaseAccessService } from './base-access.service';
 export { CollaboratorMatchAccessService } from './collaborator-match-access.service';
 export { CollaboratorAccessService } from './collaborator-access.service';
 export { CollaboratorMatchRequestAccessService } from './collaborator-match-request-access.service';
 export { ConfigurationAccessService } from './configuration-access.service';
-export { DbContextService } from './db-context.service';
 export { EventAccessService } from './event-access.service';
 export { EventFollowAccessService } from './event-follow-access.service';
 export { PaymentAccessService } from './payment-access.service';
 export { SavingAccessService } from './saving-access.service';
 export { UserAccessService } from './user-access.service';
-export { BaseAccessService } from './base-access.service';
-export {  } from './';
